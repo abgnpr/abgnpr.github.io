a7f0cad33d87bb7eaef1a87b542a3c940a378a22
mountAP <ap44122@gmail.com>
2021-02-20 01:56:09 +0530

    Layout almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,44 @@
 module.exports = {
   purge: {
-    enabled: true,
+    // enabled: true,
     content: ["./src/**/*.html"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    textColor: theme => ({
+      ...theme("colors"),
+      primary: "rgba(14,165,233,1)",
+    }),
+    backgroundColor: theme => ({
+      ...theme("colors"),
+      primary: "rgba(14,165,233,1)",
+    }),
+    maxHeight: {
+      0: "0",
+      "1/4": "25%",
+      "1/3": "33.33%",
+      "1/2": "50%",
+      "2/5": "40%",
+      "3/5": "60%",
+      "3/4": "75%",
+      full: "100%",
+    },
+    extend: {
+      boxShadow: ["active"],
+      backgroundImage: theme => ({
+        bullseye: "url('../images/bullseye.svg')",
+        palm: "url('../images/palm.jpg')",
+      }),
+      screens: {
+        portrait: { raw: "(orientation: portrait)" },
+        landscape: { raw: "(orientation: landscape)" },
+        "ratio-3/2": { raw: "(min-aspect-ratio: 3/2)" },
+        "ratio-4/3": { raw: "(min-aspect-ratio: 4/3)" },
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/aspect-ratio")],
 };
